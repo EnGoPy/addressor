c56cc4636ea07d424d9bd0b051e8ab0b6fb32cdc
Adrian Wojtasik <awojtasik@atlassian.com>
2022-08-15 00:46:02 +0200

    Add photon handshake on startup

--- src/main/java/com/engobytes/addressor/photon/ConnectionPhotonAutoSearchGateway.java
+++ src/main/java/com/engobytes/addressor/photon/ConnectionPhotonAutoSearchGateway.java
@@ -2,16 +2,21 @@ package com.engobytes.addressor.photon;
 
 import com.engobytes.addressor.configuration.LocationSearchProperty;
 import com.engobytes.addressor.photon.model.PhotonResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
 import javax.validation.constraints.NotNull;
 
 @Component
+@Slf4j
 public class ConnectionPhotonAutoSearchGateway {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConnectionPhotonAutoSearchGateway.class);
     private final RestTemplate restTemplate;
@@ -22,6 +27,35 @@ public class ConnectionPhotonAutoSearchGateway {
             @Autowired @NotNull LocationSearchProperty propertySettings) {
         this.restTemplate = restTemplate;
         this.propertySettings = propertySettings;
+    }
+
+    @PostConstruct
+    protected void handShakeWithPhoton(){
+
+        int retryCount = 5;
+        int backOff=400;
+        int probeCounter = 0;
+        boolean success;
+
+        do {
+            if(probeCounter != 0){
+                try{
+                    Thread.sleep(backOff);
+                }catch (InterruptedException e){
+                    log.warn("Thread interrupted during backOff");
+                }
+            }
+            success = successHandShakeWithPhotonInternal();
+            probeCounter++;
+            if(success) break;
+
+        }while(probeCounter <= retryCount);
+
+        if(success){
+            log.info("Successfully found photon on {}, attempts count {}", propertySettings.getSearchPhotonUrl(), probeCounter);
+        }else{
+            log.warn("Initial handshake with photon failed");
+        }
     }
 
     public PhotonResponse getLocationByGeoCoords(double lat, double lon){
@@ -61,5 +95,11 @@ public class ConnectionPhotonAutoSearchGateway {
         } else {
             return new PhotonResponse();
         }
+    }
+
+    private boolean successHandShakeWithPhotonInternal(){
+        String u = propertySettings.getSearchPhotonUrl().concat("/api?q");
+        ResponseEntity<String> response = restTemplate.getForEntity(u, String.class);
+        return HttpStatus.valueOf(response.getStatusCodeValue()).is2xxSuccessful();
     }
 }
