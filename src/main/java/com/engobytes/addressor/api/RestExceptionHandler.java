fa8cd2410a0710d9e147756fae239b457397ac8c
Adrian Wojtasik <awojtasik@atlassian.com>
2022-08-14 21:45:46 +0200

    Introduce exception handling with HTTP statuses

---
+++ src/main/java/com/engobytes/addressor/api/RestExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.engobytes.addressor.api;
+
+import com.engobytes.addressor.exception.AddressNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {AddressNotFoundException.class})
+    protected ResponseEntity<Object> handleAddressNotFound(AddressNotFoundException ex, WebRequest request){
+        return handleExceptionInternal(ex, ex.getMessage(),
+                new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+}
