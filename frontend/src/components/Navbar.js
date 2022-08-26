75e4fe02fc3a9e86af2048f5056c9bfcb7ac4063
Adrian Wojtasik <awojtasik@atlassian.com>
2022-08-26 17:18:59 +0200

    Add front end with template

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+    return(
+    <React.Fragment>
+        <ul className="navbar">
+            <li>Reversed geo parsing</li>
+            <li>Auto search</li>
+        </ul>
+    </React.Fragment>
+        );
+}
+export default Navbar;
